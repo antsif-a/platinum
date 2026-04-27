c966e1bd532ed5282e27a70badfa66baf7737e11
Андрей Анциферов <krolik0504@gmail.com>
2026-04-27 17:20:02 +0300

    fix typo

--- examples/01_hello_world.cpp
+++ examples/01_hello_world.cpp
@@ -1,7 +1,7 @@
 import std.io;
 
 int main() {
-    /* we do not namespace all library code under std:: */
+    /* we do not namespace any library code (except syscalls, which are under sys::) */
     println("Hello, World");
     return 0;
 }
